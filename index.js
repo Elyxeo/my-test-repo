abc0a80ed01a74a54f80830a67dbd842376d4e92
Elyxeo <wafi.mahdzir@gmail.com>
2021-08-24 14:54:19 +0800

    Finished working with API

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+let url = `https://restcountries.eu/rest/v2/name/Malaysia`;
+
+function waitForData(res) {
+  return res.json();
+}
+
+function handleData(data) {
+  let pop = data[0].population;
+  let name = data[0].name;
+  console.log(`The population of ${name} is ${pop}`);
+}
+
+fetch(url, { method: "GET" }).then(waitForData).then(handleData);
